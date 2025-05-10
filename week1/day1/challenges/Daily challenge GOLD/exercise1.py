a1b365c78a2558a226dd2d96407977a7a8e0bfdc
imanee11 <imaneelouannane@gmail.com>
2025-05-10 14:22:11 +0100

    complete Daily challenge GOLD for day 1

--- week1/day1/challenges/Daily challenge GOLD/exercise1.py
+++ week1/day1/challenges/Daily challenge GOLD/exercise1.py
@@ -4,13 +4,29 @@
 birthdate = input("enter ur birthdate (DD/MM/YYYY): ")
 
 
-#! The number of candles on the cake should be the last number
-#! of the users age, if they are 53, then add 3 candles.
+#! split the date 
+day, month, year = birthdate.split("/")
+# turn it into int
+year = int(year)
+# print (year)
+
+#! get today's date
+from datetime import datetime
+current_year = datetime.now().year
+# print(current_year)
+
+#! age
+age = current_year - year
+# print(age)
+
+#! get the last digit
+last_digit = age % 10
+
+#! make candles
+candles = 'i' * last_digit
 
 
-#! Display a little cake as seen below:
-
-print ('   ____iiiii____    ')
+print ('   ____'+ candles + '____')
 print ('  | :H:a:p:p:y: |  ')
 print ('__|_____________|__')
 print ('|^^^^^^^^^^^^^^^^^|')
