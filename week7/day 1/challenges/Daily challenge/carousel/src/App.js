7779d241849af7136c8d743c94f9addf96ef613c
imanee11 <imaneelouannane@gmail.com>
2025-06-16 15:48:07 +0100

    edit challenge

--- week7/day 1/challenges/Daily challenge/carousel/src/App.js
+++ week7/day 1/challenges/Daily challenge/carousel/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import DemoCarousel from './components/DemoCarousel';
+import './index.css'
 
 function App() {
   return (
-    <div className="App">
+    <div className="App container">
       <DemoCarousel />
     </div>
   );
