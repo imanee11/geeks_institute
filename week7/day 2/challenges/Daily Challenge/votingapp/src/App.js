a6c1f55b3d81ed0aeb88c2d550186d42b25cd186
imanee11 <imaneelouannane@gmail.com>
2025-06-17 15:26:53 +0100

    complete Daily Challenge: Voting app for day 2

--- week7/day 2/challenges/Daily Challenge/votingapp/src/App.js
+++ week7/day 2/challenges/Daily Challenge/votingapp/src/App.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './index.css'
 
 const App = () => {
+  const [languages, setLanguages] = useState([
+    { name: "Php", votes: 0 },
+    { name: "Python", votes: 0 },
+    { name: "JavaSript", votes: 0 },
+    { name: "Java", votes: 0 }
+  ])
+
+  const vote = (index) => {
+    const newVotes = [...languages];
+    newVotes[index].votes++;
+    setLanguages(newVotes);
+  };
+
   return (
     <>
-      
+      <div className='app'>
+        <h2>Vote your language</h2>
+        {languages.map((lang, index) => (
+          <div className='card' key={index}>
+            <div>{lang.votes}</div>
+            <div>{lang.name}</div>
+            <div><button onClick={() => vote(index)}>Vote</button></div>
+          </div>
+        ))}
+      </div>
+
+
     </>
   );
 };
